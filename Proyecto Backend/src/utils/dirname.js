73d4c5a97e528ce8ac1258c3d26a740cf53df068
klerbraian <braiankler.30@gmail.com>
2024-09-04 09:18:23 -0300

    nuevocomit

--- Proyecto Backend/src/utils/dirname.js
+++
@@ -1,8 +0,0 @@
-//CONFIGURACION PARA UTILIZAR EL METODO DIRNAME CON IMPORT
-
-import { fileURLToPath } from 'node:os'
-import { dirname } from 'node:path'
-
-
-const __filename = fileURLToPath(import.meta.url)
-export const __dirname = dirname(__filename)
